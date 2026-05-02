a4e4661365befcdffe9738eaede0affc4479f3c8
senuri perera <pereralsd@students.nsbm.ac.lk>
2026-05-02 22:15:21 +0530

    minor ui fix

--- KitchenPal-Mobile/lib/pages/login.dart
+++ KitchenPal-Mobile/lib/pages/login.dart
@@ -269,7 +269,7 @@ class _LoginPageState extends State<LoginPage> {
                               ),
                             ],
                           ),
-                          const SizedBox(height: 4),
+                          const SizedBox(height: 16),
 
                           // Login Button
                           ElevatedButton(
